3525820ddb3dfb41cb7fd81fc32890fcb4045a42
Thomas Santos <thomas@socialeffort.co>
2017-11-20 12:23:19 -0500

    require React and Api. Set up query format. Create Weather component

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,20 @@
 // ReactWeatherApp Core
+var React = require('react');
+var ReactDOM = require('react-dom');
+var classNames = require('classnames');
+var Api = require('./utils/api');
+
+var query = ";// ?city=SOME CITY HERE (ex. New York, London, Paris, Tokyo"
+var cities = []; // strings become are an array
+var citiesWeather = []; // API cache
+var currentCity = 0; // Index of current city displayed
+
+var Weather = React.createClass({
+	render: function(){
+
+	}
+});
+
+// assign React component Weather to a DOM element
+var element = React.createElement(Weather, {});
+ReactDOM.render(element, document.querySelector('.container'));
