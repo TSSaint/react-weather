d49b5204dafc496da53e43387b99bea0d3913334
Thomas Santos <thomas@socialeffort.co>
2017-11-21 23:53:32 -0500

    add api module to get data from OpenWeather api

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,2 +1,17 @@
 // Module used to interact with Weather API
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiUrl = '&appid=2de143494c0b295cca9337e1e96b00e0';
 
+module.exports = {
+    get: function(place){
+        return fetch(rootUrl + place + apiUrl, {
+            headers: {
+                // tbd
+            }
+        })
+        .then(function(response){
+            return response.json();
+        })
+    }
+}
